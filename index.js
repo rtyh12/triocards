fb8573185bda5185186b4917bba0b71bcc3abea0
Mihai Verzan <rtyh12@gmail.com>
2021-12-02 18:08:08 +0200

    Complete (maybe) room functionality and fix bugs

--- index.js
+++ index.js
@@ -69,18 +69,15 @@ class Player {
     }
 
     deal_random(count) {
-        console.log('dealing', count, 'cards')
-        console.log(this.afk);
         this.cards.length = 0;
 
         for (let i = 0; i < count; i++) {
-            this.cards[ i ] = new Card();
-            this.cards[ i ].init_random();
+            this.cards[i] = new Card();
+            this.cards[i].init_random();
         }
     }
 
     playCard(num, game) {
-        console.log(this.cards);
         if (num < 0 || num >= this.cards.length) {
             throw 'Player.playCard received illegal argument num ' + num;
         }
@@ -88,7 +85,7 @@ class Player {
         if (this.cards.length == 0) {
             this.deal_random(8);
         }
-        game.lastPlayed = removed[ 0 ];
+        game.lastPlayed = removed[0];
         return removed;
     }
 }
@@ -107,26 +104,29 @@ class Game {
         return Date.now() - this.timeCreated;
     }
 
-    player_join(name, id) {
+    player_join(name, player_id, session_id) {
         let player = new Player();
+        player.player_id = player_id;
+        player.session_id = session_id;
         player.deal_random(this.starting_card_number);
-        this.players.set(id, player);
+        this.players.set(player_id, player);
     }
 }
 
 var games = new Map();
-var checkGameValidityInterval = 1000;
-var pruneInactiveGamesAfter = 15000;
+var checkGameValidityInterval = 1 * 1000;
+var pruneInactiveGamesAfter = 5 * 60 * 1000;
+
+// this is an ugly hack, socket.io rooms would probably have been better
+var sessionToRoom = new Map();
 
 // prune old inactive games
 // TODO change to callback using setTimeout
 setInterval(() => {
-    console.log(games);
-
     games.forEach((game, key, map) => {
         if (game.getAge() > pruneInactiveGamesAfter && game.players.size == 0) {
-            console.log("Pruning game", key);
             games.delete(key);
+            console.log("Pruned room", key);
         }
     });
 }, checkGameValidityInterval);
@@ -138,59 +138,50 @@ setInterval(() => {
 // setTimeout(player.deal_random.bind(player), 5000, 8);
 // setInterval(() => console.log(player.cards.length), 1000);
 
-function send_state(game, id) {
-    console.log(game.lastPlayed);
-
-    try {
-        io.to(id).emit('receive_cards', {
-            'cards': game.players.get(id).cards,
-            'lastPlayed': game.lastPlayed
-        });
-    }
-    catch (e) {
-        console.log('Exception in send_state:\n', e);
-        io.to(id).emit('receive_cards', {
-            'cards': [],
-            'lastPlayed': new Card()
-        });
-    }
-}
-
-io.on('connection', (socket) => {
-    socket.on('click', (msg) => {
-        id = msg[ 'id' ];
-        console.log('id', id, 'clicked card', msg[ 'cardClicked' ]);
+function sendState(roomCode, player_id) {
+    let game = games.get(roomCode);
+    if (game !== undefined) {
         try {
-            game.players.get(id).playCard(msg[ 'cardClicked' ], game);
+            let player = games.get(roomCode).players.get(player_id);
+            player.cards;
+            // if neither of these threw an exception, player exists in the correct room
+            console.log('sending state to', player_id, 'in room', roomCode, 'via session', player.session_id);
 
-            game.players.forEach((value, key, map) => {
-                send_state(game, key);
+            io.to(player.session_id).emit('receive_cards', {
+                'cards': player.cards,
+                'lastPlayed': game.lastPlayed
             });
         }
         catch (e) {
-            console.log(e);
+            console.log('Exception in sendState():', e);
         }
-    });
+    }
 
-    socket.on('get_cards', (msg) => {
-        id = msg;
-        console.log('get_cards received from ' + id);
-        send_state(game, id);
-    });
+    // try {
+    //     io.to(id).emit('receive_cards', {
+    //         'cards': game.players.get(id).cards,
+    //         'lastPlayed': game.lastPlayed
+    //     });
+    // }
+    // catch (e) {
+    //     console.log('Exception in sendState:\n', e);
+    //     io.to(id).emit('receive_cards', {
+    //         'cards': [],
+    //         'lastPlayed': new Card()
+    //     });
+    // }
+}
 
-    socket.on('join_game', (msg) => {
-        roomCode = msg[ 'roomCode' ].toUpperCase();
-        id = msg['id'];
-        playerID = console.log(generatePlayerID());
-        console.log(playerID + ' (session id=' + id + ') wants to join ' + roomCode);
+io.on('connection', (socket) => {
+    socket.on('requestRoom', (data, callback) => {
+        roomCode = generateRoomCode();
+        games.set(roomCode, new Game());
+        console.log('Created room ' + roomCode);
 
-        try {
-            games.get(roomCode).player_join('noname', id);
-            send_state(games.get(msg['roomCode']), id);
-        }
-        catch (e) {
-            console.log(e);
-        }
+        callback({
+            'success': true,
+            'roomCode': roomCode
+        });
     });
 
     socket.on('isRoomcodeValid', (data, callback) => {
@@ -198,14 +189,74 @@ io.on('connection', (socket) => {
         callback(games.has(roomCode));
     });
 
-    socket.on('requestRoom', (data, callback) => {
-        roomCode = generateRoomCode();
-        games.set(roomCode, new Game());
-        console.log(games);
+    socket.on('requestJoin', (data, callback) => {
+        roomCode = data['roomCode'].toUpperCase();
+        session_id = data['session_id'];
+        player_id = generatePlayerID();
 
-        callback({
-            'success': true,
-            'roomCode': roomCode
-        });
+        console.log(player_id + ' (session id=' + session_id + ') wants to join ' + roomCode);
+
+        try {
+            games.get(roomCode).player_join('noname', player_id, session_id);
+
+            callback({
+                'approved': true,
+                'player_id': player_id,
+                'roomCode': roomCode
+            });
+
+            sendState(roomCode, player_id);
+
+            sessionToRoom.set(session_id, roomCode);
+        }
+        catch (e) {
+            console.log(e);
+
+            callback({
+                'approved': false,
+            });
+        }
+    });
+
+    socket.on('requestState', (data) => {
+        console.log('requestState received from ' + data['player_id'] + ' in room ' + data['roomCode']);
+        sendState(data['roomCode'], data['player_id']);
+    });
+
+    socket.on('click', (data) => {
+        let player_id = data['player_id'];
+        let roomCode = data['roomCode'];
+        let cardClicked = data['cardClicked'];
+
+        let game = games.get(roomCode);
+
+        console.log(player_id, 'in room', roomCode, 'wants to click card', data['cardClicked']);
+
+        try {
+            game.players.get(player_id).playCard(data['cardClicked'], game);
+            game.players.forEach((value, key, map) => {
+                sendState(roomCode, key);
+            });
+        }
+        catch (e) {
+            console.log(e);
+        }
+    });
+
+    socket.once('disconnect', (test) => {
+        if (sessionToRoom.has(socket.id)) {
+            let roomCode = sessionToRoom.get(socket.id);
+            let game = games.get(roomCode);
+
+            for (let [key, value] of game.players) {
+                if (value.session_id == socket.id) {
+                    console.log('Deleting player', game.players.get(key).player_id, 'from room', roomCode);
+                    game.players.delete(key);
+                }
+            }
+
+            sessionToRoom.delete(socket.id);
+        }
+        console.log('disconnected', socket.id);
     });
 });
